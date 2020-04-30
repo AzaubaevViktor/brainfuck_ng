6ce754051186b0f7d19d72d3cd21d2bfa926228d
Korovin Viktor <sarvash.inc@gmail.com>
2020-05-01 00:13:07 +0700

    Fix parameters with lex

--- executor/__init__.py
+++ executor/__init__.py
@@ -1,9 +1,9 @@
-from lexer import Program
+from lexer import ProgramT
 
 
 class Executor:
     def __init__(self, ns: dict):
         self.ns = ns
 
-    def __call__(self, program: Program):
+    def __call__(self, program: ProgramT):
         raise NotImplementedError()
